53754bec50a76b33565328de793d387c7475a313
Max Naumov <maxnaumovs@gmail.com>
2017-01-17 15:22:26 +0200

    .

--- PowerCoWebSite/ViewModels/LoginViewModel.cs
+++ PowerCoWebSite/ViewModels/LoginViewModel.cs
@@ -1,9 +1,13 @@
 ﻿
+using System.ComponentModel.DataAnnotations;
+
 namespace PowerCoWebSite.ViewModels
 {
     public class LoginViewModel
     {
         public string Name { get; set; }
+
+        [DataType(DataType.Password)]
         public string Password { get; set; }
     }
 }
--- PowerCoWebSite/ViewModels/NewEmployeeViewModel.cs
+++ PowerCoWebSite/ViewModels/NewEmployeeViewModel.cs
@@ -2,6 +2,7 @@
 using PowerCoWebSite.Data;
 using PowerCoWebSite.Models;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 
 namespace PowerCoWebSite.ViewModels
@@ -9,10 +10,14 @@ namespace PowerCoWebSite.ViewModels
     public class NewEmployeeViewModel
     {
         public int EmployeeId { get; set; }
+
+        [Required, MaxLength(64)]
         public string FullName { get; set; }
         public string DepartmentName { get; set; }
         public string Position { get; set; }
         public string Head { get; set; }
+
+        [Required]
         public double Salary { get; set; }
         public List<Department> Departments { get; set; }
         public List<EmployeePosition> Positions { get; set; }
